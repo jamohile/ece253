d853bc01497068014f3d82d2dcd85c4acb0c1c60
rafayk7 <rafayk7@yahoo.com>
2019-09-26 21:10:01 -0400

    fix semicolon

--- Lab 2/2.v
+++ Lab 2/2.v
@@ -7,7 +7,7 @@ module mux4bit2to1(X, Y, s, M);
     assign M[1] = (!s & X[1]) | (s & Y[1]);
     assign M[2] = (!s & X[2]) | (s & Y[2]);
     assign M[3] = (!s & X[3]) | (s & Y[3]);
-endmodule;
+endmodule
 
 module part2 (SW, LEDR);
     input [9:0] SW;
@@ -20,8 +20,8 @@ module part2 (SW, LEDR);
     assign Y = SW[4:7];
     assign s = SW[9];
 
-    mux4bit2to1 U (X, Y, sw[9], LEDR[3:0]);
-endmodule;
+    mux4bit2to1 U (X, Y, SW[9], LEDR[3:0]);
+endmodule
 
 
 
