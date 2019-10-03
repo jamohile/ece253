af247d69c1487c3aa6a7a0401bfed324788600f1
Jay Mohile <jamohile@gmail.com>
2019-10-03 14:01:30 -0400

    Added parts 4 and 5

---
+++ Lab 2:3/4.v
@@ -0,0 +1,23 @@
+module cToHex(c, HEX0);
+    input [6:0] HEX0;
+    input [1:0] c;
+
+    assign HEX0[0] = 1b'0;
+    assign HEX0[1] = c1;
+    assign HEX0[2] = !c1;
+    assign HEX0[3] = 1b'0;
+    assign HEX0[4] = !(c1 + c2);
+    assign HEX0[5] = !(c1 + c2);
+    assign HEX0[6] = c1 & c1;
+endmodule;
+
+module part4(SW, HEX0);
+    input [9:0] SW;
+    input [6:0] HEX0;
+
+    wire [1:0] c;
+    assign c = SW[1:0];
+
+    cToHex U (c, HEX0);
+endmodule;
+
---
+++ Lab 2:3/5.v
@@ -0,0 +1,18 @@
+module part4(SW, HEX0);
+    input [9:0] SW;
+    input [6:0] HEX0;
+
+    wire [1:0] X, Y, Z, s, c;
+
+    assign s = SW[9:8];
+
+    assign X = SW[5:4];
+    assign Y = SW[3:2];
+    assign Z = SW[1:0];
+
+
+    mux2bit3to1 U (X, Y, Z, s, c);
+    cToHex U (c, HEX0);
+endmodule;
+
+
